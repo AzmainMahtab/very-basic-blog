d78b4612edde89f9740d5b505ae284f268ff073d
Azmain Mahtab <azmainmahtab97@gmail.com>
2020-09-07 04:04:35 +0600

    after adding blog date (experiment)

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 class Blog(models.Model):
 	title = models.CharField(max_length=100)
+	description = models.CharField(max_length=100, blank=True)
 	article = models.TextField(max_length=1000)
 	author = models.CharField(max_length=50)
+	date = models.DateTimeField(default=now, null=True)
